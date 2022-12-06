998dedfaff61e9d2ce311b22abc9c0d27f867ada
Patryk Przekwas <patryk@kubermatic.com>
2022-12-06 07:13:06 +0100

    Pin nix channels

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,12 +1,49 @@
-{ config, pkgs, libs, ... }:
+{ config, ... }:
 
-{
+let
+  username = builtins.getEnv "USER";
+  homeDir = builtins.getEnv "HOME";
+
+  # pin stable and unstable channels to specific commits
+  stable = "527bba34acb1235fed50eab49c627f14de15cc55";
+  unstable = "a2d2f70b82ada0eadbcb1df2bca32d841a3c1bf1";
+
+  pkgs = import (builtins.fetchGit {
+    name = "nixpkgs-stable";
+    url = "https://github.com/nixos/nixpkgs.git";
+    ref = "refs/heads/nixpkgs-22.11-darwin";
+    rev = "${stable}";
+  }) { config = { allowUnfree = true; }; };
+
+  pkgs_unstable = import (builtins.fetchGit {
+    name = "nixpkgs-unstable";
+    url = "https://github.com/nixos/nixpkgs.git";
+    ref = "refs/heads/nixpkgs-unstable";
+    rev = "${unstable}";
+  }) {
+    config = { allowUnfree = true; };
+    overlays = [
+      (self: super: {
+
+      })
+    ];
+  };
+
+in {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  home.username = "patrykprzekwas";
-  home.homeDirectory = "/Users/patrykprzekwas";
+  home.username = username;
+  home.homeDirectory = homeDir;
 
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
   home.stateVersion = "22.11";
 
   home.packages = with pkgs; [
@@ -27,5 +64,4 @@
     zsh
     tmux
   ];
-
 }
