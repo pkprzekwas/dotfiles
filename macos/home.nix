5c087ea9356cd73dd9f0870695860810ccab7950
Patryk Przekwas <patryk@kubermatic.com>
2022-12-31 15:13:55 +0100

    Add tmux conf to macos

--- macos/home.nix
+++ macos/home.nix
@@ -50,14 +50,12 @@ in {
     bottom
     tree
     ripgrep
-    git
     jq
     tldr
     tree
     wget
     bat
     fzf
-    tmux
     gnused
     diff-so-fancy
     exa
@@ -70,7 +68,6 @@ in {
     vault
     libiconv
 
-    go
     rustup
     gcc
   ];
@@ -83,11 +80,25 @@ in {
     LC_CTYPE = "en_US.UTF-8";
     LC_ALL = "en_US.UTF-8";
     EDITOR = "nvim";
-    PAGER = "less -FirSwX";
     MANPAGER = "sh -c 'col -bx | ${pkgs.bat}/bin/bat -l man -p'";
   };
 
+  # TODO: move tmux config to shared directory.
+  home.file.".tmux.conf".source = ../vms/users/pprzekwa/tmux-conf;
+  home.file.".tmux.conf.local".source = ../vms/users/pprzekwa/tmux-conf-local;
+
   ### Programs
+
+  programs.go = {
+    enable = true;
+    goPath = "code/go";
+    goPrivate = [ "github.com/pkprzekwas" ];
+  };
+
+  programs.tmux = {
+    enable = true;
+  };
+
   programs.zsh = {
     enable = true;
 
@@ -101,7 +112,6 @@ in {
 
     shellAliases = {
       ll = "exa -l";
-      lll = "gls -lahF --group-directories-first";
 
       ga = "git add";
       gc = "git commit";
@@ -118,6 +128,29 @@ in {
       kgd = "kubectl get deployments";
       ks = "kubectl -n kube-system";
       kcd = "kubectl config set-context $(kubectl config current-context) --namespace ";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Patryk Przekwas";
+    userEmail = "patryk@kubermatic.com";
+    aliases = {
+      prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+      tree = "log --graph --decorate --pretty=oneline --abbrev-commit";
+      root = "rev-parse --show-toplevel";
+    };
+    extraConfig = {
+      branch.autosetuprebase = "always";
+      color.ui = true;
+      credential.helper = "store"; # want to make this more secure
+      github.user = "pkprzekwas";
+      push.default = "tracking";
+      init.defaultBranch = "main";
+      core = {
+        askPass = ""; # needs to be empty to use terminal for ask pass
+        editor = "nvim";
+      };
     };
   };
 
